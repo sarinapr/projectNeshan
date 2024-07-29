7d7cd6a631b8f71e5d1652b276fd3037aa759c6b
sarina.pourattar <sarina.pourattar@partsoftware.com>
2024-07-29 12:57:20 +0430

    add new color

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstproject.ui.theme.FirstProjectTheme
 
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier, color = Color.LightGray
     )
 }
 
